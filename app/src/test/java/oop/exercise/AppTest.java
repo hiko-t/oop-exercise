76788c48a39679b6f2331516566207bfab32e18c
hiko_t <yotti4415@gmail.com>
2025-01-19 23:46:39 +0900

    Add sample code

--- app/src/test/java/oop/exercise/AppTest.java
+++ app/src/test/java/oop/exercise/AppTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test void getTicketPrice() {
         App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertEquals(0, classUnderTest.getTicketPrice(1, 2));
     }
 }
